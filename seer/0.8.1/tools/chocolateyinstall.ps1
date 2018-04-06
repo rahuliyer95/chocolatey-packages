4a0d6873b7fe3263b4f465f6dbaaf307d74b0db4
rahuliyer95 <rahuliyer573@gmail.com>
2018-04-07 00:02:58 +0530

    Added seer v0.8.1

---
+++ seer/0.8.1/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$installFile = Join-Path $tools 'Seer-0.8.1.exe'
+ 
+$packageArgs = @{
+  packageName    = $packageName
+  file           = $installFile
+  fileType       = 'EXE'
+  validExitCodes = @(0, 3010, 1641)
+  silentArgs     = '/S'
+  softwareName   = 'Seer'
+  checksum       = 'DB8EDA0E96ECA6457886F353D3B531D234DA5FFC4CFB92D103B5417F9FDBC4B8'
+  checksumType   = 'SHA256'
+}
+ 
+Install-ChocolateyPackage @packageArgs
+Remove-Item "$installFile"
---
+++ seer/README.md
@@ -0,0 +1,8 @@
+﻿# Seer
+
+Installation script for [seer](https://1218.io)
+
+## Building
+
+Execute `choco pack` to generate the `nupkg` file that can be tested
+
