5ff915ef32a72f75b7e26a4b35a1210084a5312d
rahuliyer95 <rahuliyer573@gmail.com>
2018-04-06 23:20:04 +0530

    Added inter-ui font

---
+++ inter-ui/2.5/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$package = Join-Path (Split-Path $tools) "fonts"
+ 
+function Get-CurrentDirectory
+{
+  $thisName = $MyInvocation.MyCommand.Name
+  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
+Install-ChocolateyZipPackage `
+  -PackageName "$env:ChocolateyPackageName" `
+  -Url 'https://github.com/rsms/inter/releases/download/v2.5/Inter-UI-2.5.zip' `
+  -Checksum '0A8A74623D254FA6DBF92EF8C564E310727013B2A2CF701F76F448568F7D05E7' `
+  -ChecksumType 'SHA256' `
+  -UnzipLocation $package
+ 
+$AddFont = Join-Path (Get-CurrentDirectory) 'Add-Font.ps1'
+& $AddFont -Path "$package\Inter UI (OTF)"
+Remove-Item -Recurse -Force $package
